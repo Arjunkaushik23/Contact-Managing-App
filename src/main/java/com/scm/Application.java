fdb2c4ca118f712aa5a9ab748c31810784f41b7f
Arjun Kaushik <88938626+Arjunkaushik23@users.noreply.github.com>
2024-06-09 17:25:38 +0530

    again commit for new update for testing purpose

--- src/main/java/com/scm/Application.java
+++ src/main/java/com/scm/Application.java
@@ -8,8 +8,6 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-		System.out.println("This app is for me");
-		System.out.println("This is for testing that git is working or not:::::");
 	}
 
 }
