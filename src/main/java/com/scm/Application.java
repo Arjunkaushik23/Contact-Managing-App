cfe275329ad485b2614ea74ca2d92522738c7a61
Arjun Kaushik <88938626+Arjunkaushik23@users.noreply.github.com>
2024-06-09 17:23:43 +0530

    adding this commit for testing only

--- src/main/java/com/scm/Application.java
+++ src/main/java/com/scm/Application.java
@@ -9,6 +9,7 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 		System.out.println("This app is for me");
+		System.out.println("This is for testing that git is working or not:::::");
 	}
 
 }
