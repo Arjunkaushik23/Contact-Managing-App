8b60ae49c99d44011962c07e39c1ebef0dfe2f93
Arjun Kaushik <88938626+Arjunkaushik23@users.noreply.github.com>
2024-06-14 12:23:04 +0530

    testing

--- src/main/java/com/scm/Application.java
+++ src/main/java/com/scm/Application.java
@@ -9,7 +9,7 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 
-		System.out.println("Hello This is my application");
+		System.out.println("Build started or not?");
 	}
 
 }
